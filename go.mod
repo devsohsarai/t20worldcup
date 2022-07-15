00cd26f070bdf73d9fc963c66c3262ff29ec2d86
Ravinesh <kumar5891ravinesh@gmail.com>
2022-07-14 18:47:46 -0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/devsohsarai/t20worldcup
+
+go 1.18
---
+++ t20worldcup.go
@@ -0,0 +1,19 @@
+package t20worldcup
+
+import "fmt"
+
+func India() {
+	fmt.Println("Rohit Sharma will lead the t20worldcup!")
+}
+
+func Pakistan() {
+	fmt.Println("Babar Azam will lead the t20worldcup!")
+}
+
+func Australia() {
+	fmt.Println("Ricky ponting will lead the t20worldcup!")
+}
+
+func England() {
+	fmt.Println("Jos butler will lead the t20worldcup!")
+}
